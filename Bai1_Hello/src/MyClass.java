906b4d49cfe3829a4ab79b1e681ab484f7277c6f
minthuw885 <thuw882005@gmail.com>
2024-09-23 20:17:22 +0700

    them class

---
+++ Bai1_Hello/src/MyClass.java
@@ -0,0 +1,19 @@
+
+public class MyClass {
+		public static void main(String[] args) {
+
+		// Khai báo các biến
+
+		int a =5;
+
+		int b = 7;
+
+		int c = a + b;
+
+		// In ra màn hình
+
+		System.out.println("HÔM NAY TÔI BẮT ĐẦU HỌC JAVA"); System.out.println("Tổng là:" + c);
+
+		}
+
+}
