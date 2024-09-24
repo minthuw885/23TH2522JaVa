b48a0acff4cd7934c0dbf03528881a39f79f0b41
minthuw885 <thuw882005@gmail.com>
2024-09-24 19:50:29 +0700

    code

--- Bai2_NhapXuat/src/thomc/BaiCoBan/NhapXuatHoTen.java
+++ Bai2_NhapXuat/src/thomc/BaiCoBan/NhapXuatHoTen.java
@@ -5,34 +5,26 @@ import java.util.Scanner;
 public class NhapXuatHoTen {
 	public static void main(String[] args) {
 		System.out.println("-------NHAP XUAT-------");
-		
-		//Khai báo các biến chứa dữ liệu nhập vào
 		String hoTen;
 		int tuoi;
 		double chieuCao;
 		double canNang;
-		// Tạo đối tượng của lớp Scanner để nhập dữ liệu
-		Scanner banPhim; // khai báo
-		banPhim = new Scanner(System.in);// xin mới
-		// Đọc dữ liệu từ bàn phím, cất vào các biến tương ứng
-		
-		// In ra hướng dẫn cho User biết,cần làm gì:
-		System.out.print("Họ tên? ");
+		Scanner banPhim;
+		banPhim = new Scanner(System.in);
+		System.out.print("Họ va tên: ");
 		hoTen= banPhim.nextLine(); 
-		System.out.print("Tuổi? ");
+		System.out.print("Tuổi: ");
 		tuoi = banPhim.nextInt();
-		System.out.print("Chiều cao (m)? ");
+		System.out.print("Chiều cao (m): ");
 		chieuCao = banPhim.nextDouble();
-		System.out.print("Cân nặng (kg)? ");
-		canNang = banPhim.nextDouble();
-		
-		// Xuất thông tin
-		System.out.println("===========================");
+		System.out.print("Cân nặng (kg): ");
+		canNang = banPhim.nextDouble();		
+		System.out.println("__________________");
 		System.out.println("Name: " + hoTen);
 		System.out.println("Age: " + tuoi);
 		System.out.println("Weight: " + canNang);
 		System.out.println("Height: " + chieuCao);
-		System.out.println("===========================");
+
 		
 	}
 
