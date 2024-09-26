8b0eb864b4b2c2a6ed1b1a055f553cc5bf09bdbe
minthuw885 <thuw882005@gmail.com>
2024-09-26 20:34:36 +0700

    code

---
+++ Lab2/src/Bai3_Tinhtiendien.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+	public class Bai3_Tinhtiendien {
+	    public static void main(String[] args) {
+	        try (Scanner scanner = new Scanner(System.in)) {
+				System.out.print("Nhập số điện sử dụng: ");
+				int soDien = scanner.nextInt();
+				int tien;
+				if (soDien <= 50) {
+				    tien = soDien * 1000;
+				} else {
+				    tien = 50 * 1000 + (soDien - 50) * 1200;
+				}
+				System.out.println("Tiền điện phải trả: " + tien + " VND");
+			}
+	    
+	}
+}
